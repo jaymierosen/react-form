15cc30165a5dcd91e108f9a809542ed918367afa
jaymierosen <rosen.jaymie@gmail.com>
2019-02-12 09:06:55 -0500

    Updates

---
+++ precache-manifest.e7ac39b9efc493051edaf65183abb962.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1137e30241298837d770",
+    "url": "/react-form/static/js/runtime~main.1137e302.js"
+  },
+  {
+    "revision": "363ba0cb9bf59902aaf3",
+    "url": "/react-form/static/js/main.363ba0cb.chunk.js"
+  },
+  {
+    "revision": "6f146bac98f2315d5f9b",
+    "url": "/react-form/static/js/1.6f146bac.chunk.js"
+  },
+  {
+    "revision": "363ba0cb9bf59902aaf3",
+    "url": "/react-form/static/css/main.0a2886e8.chunk.css"
+  },
+  {
+    "revision": "56d4771f0f67ca410f6d159f656a153e",
+    "url": "/react-form/index.html"
+  }
+];
--- src/Components/StyledComponents/StyledComponents.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Title = styled.h1`
-font-size: 2em;
-color: #33c0b4;
-`;
